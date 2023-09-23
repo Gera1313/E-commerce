5112ca6c9063111197ea8c067fcb25d9e535a833
Mark Anthony <touya1313@hotmail.com>
2023-09-22 18:09:52 -0600

    connected env

--- Develop/server.js
+++ Develop/server.js
@@ -14,3 +14,6 @@ app.use(routes);
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
 });
+
+//gerardoperez@Gerardos-Air Develop % npm install
+// npm WARN deprecated sequelize@5.22.5: Please update to v6 or higher! A migration guide can be found here: https://sequelize.org/v6/manual/upgrade-to-v6.html
