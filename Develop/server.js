4a9d3a1ade42e1720005e269df2625a747916e8b
Mark Anthony <touya1313@hotmail.com>
2024-03-04 21:17:06 -0700

    starting to fix my current code

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
 // import sequelize connection
-const sequelize = require('./config/connection');
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -21,10 +21,5 @@ app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
 sequelize.sync({ force: false }).then(() => {
-app.listen(PORT, () => 
-  console.log(`App listening on port ${PORT}!`)
-  );
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
 });
-
-//gerardoperez@Gerardos-Air Develop % npm install
-// npm WARN deprecated sequelize@5.22.5: Please update to v6 or higher! A migration guide can be found here: https://sequelize.org/v6/manual/upgrade-to-v6.html
