1bc06a2c65f0fefbef192a724b30aec2457967bf
Mark Anthony <touya1313@hotmail.com>
2024-03-10 20:49:24 -0600

    finished router.delete

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -63,7 +63,16 @@ router.delete('/:id', async (req, res) => {
             id: req.params.id
         }
     });
-  }
-});
+
+    if (!categoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+  
+      res.status(200).json(categoryData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 module.exports = router;
